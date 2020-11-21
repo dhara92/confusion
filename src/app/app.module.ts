3c312439d7e550f71f123eeda7e8d2792bd0c78a
dharanarola2012@gmail.com <dharanarola2012@gmail.com>
2020-11-21 15:56:49 -0500

    heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/Toolbar';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatListModule} from '@angular/material/list';
 import { AppComponent } from './app.component';
