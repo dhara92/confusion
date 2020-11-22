64278f77b6032ba60b744989db4d783e7e9c5ce2
dharanarola2012@gmail.com <dharanarola2012@gmail.com>
2020-11-21 20:19:24 -0500

    heroku

--- src/server.js
+++ src/server.js
@@ -7,10 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/conFusion'));
 
-app.get('/*', function (req, res) {
+/*app.get('/*', function (req, res) {
 
     res.sendFile(path.join(__dirname + '/dist/conFusion/index.html'));
-});
+});*/
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 3000);
